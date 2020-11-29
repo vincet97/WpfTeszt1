c49d20a859f160b896b8d93ec0dd1595f3764a57
Vince Tóth <vincetot97@gmail.com>
2020-11-29 16:50:17 +0100

    Bindolás oké, Ujat is ad hozzá valahogy, Updateli az össze értéket

--- WpfTeszt1/Views/MainWindow.xaml.cs
+++ WpfTeszt1/Views/MainWindow.xaml.cs
@@ -32,19 +32,6 @@ namespace WpfTeszt1.Views
             this.DataContext = ViewModel;
         }
 
-        private void ModifCombo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-         //   System.Diagnostics.Debug.WriteLine("Az Argumentek");
-         //   System.Diagnostics.Debug.WriteLine( ViewModel.ToString());
-           
-        }
-
-        private void ModifCombo2_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-         //   System.Diagnostics.Debug.WriteLine("Az Argumentek");
-           // System.Diagnostics.Debug.WriteLine(ViewModel.ToString());
-        }
-
         private void SpeedCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
            // System.Diagnostics.Debug.WriteLine("Az Argumentek");
@@ -52,40 +39,11 @@ namespace WpfTeszt1.Views
             
             
             
-          //  System.Diagnostics.Debug.WriteLine(ViewModel.ToString());
+           System.Diagnostics.Debug.WriteLine(ViewModel.ToString());
            // System.Diagnostics.Debug.WriteLine(str);
            // System.Diagnostics.Debug.WriteLine(e.OriginalSource.ToString());
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            DataGrid dataGrid = sender as DataGrid;
-            if (e.AddedItems != null && e.AddedItems.Count > 0)
-            {
-                // find row for the first selected item
-                DataGridRow row = (DataGridRow)dataGrid.ItemContainerGenerator.ContainerFromItem(e.AddedItems[0]);
-                if (row != null)
-                {
-                   
-                    
-                }
-            }
-            //System.Diagnostics.Debug.WriteLine(cellV);
-            //System.Diagnostics.Debug.WriteLine(sender.GetType());
-            System.Diagnostics.Debug.WriteLine("Lefutott a Teljes grid event");
-        }
-        static T GetVisualChild<T>(Visual parent) where T : Visual
-        {
-            T child = default(T);
-            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
-            for (int i = 0; i < numVisuals; i++)
-            {
-                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
-                child = v as T;
-                if (child == null) child = GetVisualChild<T>(v);
-                if (child != null) break;
-            }
-            return child;
-        }
+       
     }
 }
